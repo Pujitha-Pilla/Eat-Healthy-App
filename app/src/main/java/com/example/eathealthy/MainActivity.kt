77ec7f431f97d07d01c583870934db1fb504be8f
Pujitha Pilla <poojasuhana8@gmail.com>
2021-03-16 20:22:21 +0530

    Added navigation drawer

--- app/src/main/java/com/example/eathealthy/MainActivity.kt
+++ app/src/main/java/com/example/eathealthy/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.eathealthy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainToolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainToolbar = findViewById(R.id.mainToolbar)
     }
 }
